3aa3d4d7b4613c39300476cd7b93c08ff2956d49
今永翔太 <imanagashota@shota-MC-Pro.local>
2021-08-19 06:51:57 +0900

    feat:ルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'documents/index'
-  get 'documents/show'
-  get 'documents/new'
-  get 'documents/create'
-  get 'documents/edit'
-  get 'documents/update'
-  get 'documents/destroy'
+  root "documents#index"
+  resources :documents
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

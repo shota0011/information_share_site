1960f22e467a2a48358cbab2c4bac2c40af9df65
今永翔太 <imanagashota@shota-MC-Pro.local>
2021-08-20 16:34:07 +0900

    feat:記事詳細機能を実装

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -3,12 +3,16 @@ class DocumentsController < ApplicationController
   end
 
   def show
+    @document = Document.find(params[:id])
   end
 
   def new
+    @document = Document.new
   end
 
   def create
+    document = Document.create!(document_params)
+    redirect_to document
   end
 
   def edit
@@ -19,4 +23,11 @@ class DocumentsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def document_params
+    params.require(:document).permit(:title, :body)
+  end
+
 end

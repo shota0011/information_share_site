854be4dc348b7410b19e627f0b992ed2f0d3f987
今永翔太 <imanagashota@shota-MC-Pro.local>
2021-09-14 21:39:30 +0900

    refactor:リダイレクトを削除し、before_actionを追記

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,18 @@
 class LikesController < ApplicationController
+  before_action :set_like, only: %i[create destroy]
 
   def create
-    current_user.likes.create!(document_id: params[:document_id])
-    redirect_back(fallback_location: root_path)
+    current_user.likes.create!(document_id: @document.id)
   end
 
   def destroy
-    current_user.likes.find_by(document_id: params[:document_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    current_user.likes.find_by(document_id: @document.id).destroy!
   end
+
+  private
+
+  def set_like
+    @document = Document.find(params[:document_id])
+  end
+
 end

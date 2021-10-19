41492b39110ce027a2c33c437810757d4a4e6c7d
今永翔太 <imanagashota@shota-MC-Pro.local>
2021-10-19 12:56:55 +0900

    feat:ゲストログイン用のルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :documents do
     resource :likes, only: [:create, :destroy]
   end
+  devise_scope :users do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
 end
